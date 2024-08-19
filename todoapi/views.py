6b527d3fe501ad970d90877da970bdeab1eace48
SpursFan21 <duncanfrederic@gmail.com>
2024-08-20 10:41:50 +1200

    views update

--- todoapi/views.py
+++ todoapi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
- 
+from rest_framework import generics
 from todoapi.models import Todo
 from todoapi.serializers import TodoAllDetailsSerializer, TodoSummarySerializer
 
