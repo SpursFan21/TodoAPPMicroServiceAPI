f35bbebede8d679b658f068de90f425e81e4c8be
SpursFan21 <duncanfrederic@gmail.com>
2024-08-20 09:26:31 +1200

    update

--- TodoAPPMicroServiceAPI/settings.py
+++ TodoAPPMicroServiceAPI/settings.py
@@ -75,11 +75,25 @@ WSGI_APPLICATION = 'TodoAPPMicroServiceAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'DjangoMicroAPI',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',#'This is the password for your local postgres pgadmin'
+
+        'HOST': '', #'Localhost is empty'
+
+        'PORT':'', #Assumes default as 5432
+
     }
+
 }
+ 
 
 
 # Password validation
