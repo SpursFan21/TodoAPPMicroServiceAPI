4f546a11b3c839764b0e8740e09fa5ce48979517
SpursFan21 <duncanfrederic@gmail.com>
2024-08-23 09:08:18 +1200

    update to views todo

--- todoapi/views.py
+++ todoapi/views.py
@@ -1,11 +1,23 @@
 from rest_framework import generics
 from todoapi.models import Todo
 from todoapi.serializers import TodoAllDetailsSerializer, TodoSummarySerializer
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+# Create your views here.
+#List create API view allows us to POST i.e. create a new object
+# Or list all objects
 class ListTodo(generics.ListCreateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSummarySerializer  # Or TodoAllDetailsSerializer if you need more fields
-
+	authentication_classes = [SessionAuthentication, BasicAuthentication]
+	permission_classes = [IsAuthenticated]	
+	queryset = Todo.objects.all()
+	serializer_class = TodoAllDetailsSerializer
+ 
+# This API view allows us to do GET by id,
+# UPDATE i.e. PUT by specifying the id of the todo-task
+# DELETE i.e. DELETE the todo-ask we want to delete by specifying the id of the task
 class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoAllDetailsSerializer
+	authentication_classes = [SessionAuthentication, BasicAuthentication]
+	permission_classes = [IsAuthenticated]	
+	queryset = Todo.objects.all()
+	serializer_class = TodoAllDetailsSerializer
